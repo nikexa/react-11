18e16d53865fe5be7385bddd53339397760e46de
nikoloz <nikusha.tskhvaro@gmail.com>
2025-02-10 00:07:38 +0400

    maiun

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,18 @@ import { useState } from 'react'
     background-size: cover;
     display: flex;
     position: relative;
+    @media (max-width:1200px) {
+      flex-direction: column;
+    }
   `
   const LeftDiv = styled.div`
     width: 100%;
     display: flex;
     flex-direction: column;
     align-items: end;
+    @media (max-width:1200px) {
+      align-items: start;
+    }
   `
   const LeftDivForP=styled.div`
     width: 540px;
@@ -29,6 +35,13 @@ import { useState } from 'react'
     display: flex;
     justify-content: end;
     gap: 20px;
+    @media (max-width:1200px) {
+      margin-left: 120px;
+    }
+    @media (max-width:750px) {
+      margin-left: 30px;
+      width: 325px;
+    }
   `
   const SecondDivForP=styled.div`
     
@@ -44,6 +57,10 @@ import { useState } from 'react'
     font-weight: 400;
     color: white;
     width: 540px;
+    @media (max-width:750px) {
+      width: 290px;
+      font-size: 12px;
+    }
   `
   const LeftDivPAuthor=styled.p`
     font-family: Inter;
@@ -51,11 +68,22 @@ import { useState } from 'react'
     font-weight: 700;
     color: white;
     margin-top: 20px;
+    @media (max-width:750px) {
+      font-size: 12px;
+    }
   `
   const LeftDivBottom=styled.div`
     width: 575px;
     height: 288px;
     margin-top: 300px;
+    @media (max-width:1200px) {
+      width: 500px;
+      margin-left: 120px;
+    }
+    @media (max-width:750px) {
+      width: 278px;
+      margin-left: 40px;
+    }
   `
   const DivForRqagac=styled.div`
     display: flex;
@@ -71,6 +99,12 @@ import { useState } from 'react'
     letter-spacing: 4px;
     font-family: inter;
     color: white;
+    @media (max-width:1200px) {
+      font-size: 18px;
+    }
+    @media (max-width:750px) {
+      font-size: 15px;
+    }
   `
   const DivForTime=styled.div`
     width: 575px;
@@ -79,6 +113,13 @@ import { useState } from 'react'
     align-items: end;
     gap: 20px;
     margin-top: 20px;
+    @media (max-width:1200px) {
+      width: 500px;
+    }
+    @media (max-width:750px) {
+      width: 278px;
+      height: 150px;
+    }
   `
   const TimeP=styled.p`
     font-size: 200px;
@@ -87,6 +128,12 @@ import { useState } from 'react'
     color: white;
     align-self: center;
     letter-spacing: 8px;
+    @media (max-width:1200px) {
+      font-size: 175px;
+    }
+    @media (max-width:750px) {
+      font-size: 100px;
+    }
   `
   const BST=styled.p`
     font-size: 40px;
@@ -94,6 +141,13 @@ import { useState } from 'react'
     font-weight: 300;
     color: white;
     padding-bottom: 15px;
+    @media (max-width:1200px) {
+      font-size: 32px;
+    }
+    @media (max-width:750px) {
+      font-size: 15px;
+      padding-bottom: 30px;
+    }
   `
   const City=styled.p`
     font-family: inter;
@@ -102,12 +156,24 @@ import { useState } from 'react'
     color: white;
     letter-spacing: 6px;
     margin-top: 20px;
+    @media (max-width:1200px) {
+      font-size: 18px;
+    }
+    @media (max-width:750px) {
+      font-size: 15px;
+    }
   `
     const RightDiv = styled.div`
     width: 100%;
     display: flex;
     justify-content: end;
     align-items: end;
+    @media (max-width:1200px) {
+      justify-content: start;
+      align-items: start;
+      margin-top: 60px !important;
+    }
+    
   `
   const RightBTN=styled.button`
     width: 146px;
@@ -121,6 +187,15 @@ import { useState } from 'react'
     gap: 15px;
     margin-right: 200px;
     margin-bottom: 150px;
+    @media (max-width:1200px) {
+      margin: 0;
+      margin-left: 120px;
+    }
+    @media (max-width:750px) {
+      width: 115px;
+      height: 39px;
+      margin-left: 40px;
+    }
   `
   const RightBtnP=styled.p`
     font-family: inter;
@@ -129,12 +204,19 @@ import { useState } from 'react'
     color: black;
     opacity: 50%;
     letter-spacing: 1px;
+    @media (max-width:750px) {
+      font-size: 12px;
+    }
   `
 
 
   const RightBtnIMG=styled.img`
     width: 40px;
     height: 40px;
+    @media (max-width:750px) {
+      width: 32px;
+      height: 32px;
+    }
   `
   const BottomDiv=styled.div`
     width: 100%;
@@ -144,6 +226,10 @@ import { useState } from 'react'
     position: absolute;
     display: flex;
     align-items: center;
+    @media (max-width:750px) {
+      /* height: 256px; */
+      flex-direction: column;
+    }
   `
   const BottomLeftDiv=styled.div`
     width: 100%;
@@ -151,6 +237,11 @@ import { useState } from 'react'
     display: flex;
     flex-direction: column;
     align-items: center;
+    @media (max-width:750px) {
+      height: 56px;
+      margin-top: 40px;
+      gap: 10px;
+    }
   `
 
   const BottomLinetDiv=styled.div`
@@ -159,30 +250,81 @@ import { useState } from 'react'
     background-color: #303030;
     opacity: 25%;
     margin-right: 100px;
+    @media (max-width:750px) {
+      display: none;
+    }
   `
 
 
   const BottomRightDiv=styled.div`
     width: 90%;
     height: 252px;
+    @media (max-width:750px) {
+      height: 56px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-top: 15px;
+      gap: 10px;
+    }
   `
   const TimeZoneLeftDiv=styled.div`
     width: 423px;
     height: 105px;
+    @media (max-width:1200px) {
+      width: 303px;
+    }
+    @media (max-width:750px) {
+      width: 323px;
+      height: 28px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
   `
   const DayOfYear=styled.div`
     width: 423px;
     height: 105px;
+    @media (max-width:1200px) {
+      width: 303px;
+    }
+    @media (max-width:750px) {
+      width: 323px;
+      height: 28px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
   `
 
   const DayOfWeek=styled.div`
     width: 179px;
     height: 105px;
+    @media (max-width:1200px) {
+      width: 160px;
+    }
+    @media (max-width:750px) {
+      width: 323px;
+      height: 28px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
   `
 
   const WeekNumber=styled.div`
     width: 179px;
     height: 105px;
+    @media (max-width:1200px) {
+      width: 155px;
+    }
+    @media (max-width:750px) {
+      width: 323px;
+      height: 28px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
   `
   const TimeZoneTopP=styled.p`
     font-family: inter;
@@ -191,12 +333,25 @@ import { useState } from 'react'
     letter-spacing: 4px;
     color: #303030;
     margin-bottom: 15px;
+    @media (max-width:1200px) {
+      font-size: 13px;
+    }
+    @media (max-width:750px) {
+      font-size: 10px;
+      margin: 0;
+    }
   `
   const TimeZoneBottomP=styled.p`
     font-family: inter;
     font-size: 56px;
     font-weight: 700;
     color: #303030;
+    @media (max-width:1200px) {
+      font-size: 40px;
+    }
+    @media (max-width:750px) {
+      font-size: 20px;
+    }
   `
   const DayOfYearTopP=styled.p`
     font-family: inter;
@@ -206,12 +361,26 @@ import { useState } from 'react'
     color: #303030;
     margin-top: 50px;
     margin-bottom: 15px;
+    @media (max-width:1200px) {
+      font-size: 13px;
+    }
+    @media (max-width:750px) {
+      font-size: 10px;
+      margin: 0;
+    }
   `
   const DayOfYearBottomP=styled.p`
     font-family: inter;
     font-size: 56px;
     font-weight: 700;
     color: #303030;
+    @media (max-width:1200px) {
+      font-size: 40px;
+      
+    }
+    @media (max-width:750px) {
+      font-size: 20px;
+    }
   `
   const DayOfWeekTopP=styled.p`
     font-family: inter;
@@ -220,12 +389,25 @@ import { useState } from 'react'
     letter-spacing: 4px;
     color: #303030;
     margin-bottom: 15px;
+    @media (max-width:1200px) {
+      font-size: 13px;
+    }
+    @media (max-width:750px) {
+      font-size: 10px;
+      margin: 0;
+    }
   `
   const DayOfWeekBottomP=styled.p`
     font-family: inter;
     font-size: 56px;
     font-weight: 700;
     color: #303030;
+    @media (max-width:1200px) {
+      font-size: 40px;
+    }
+    @media (max-width:750px) {
+      font-size: 20px;
+    }
   `
   const WeekNumberTopP=styled.p`
     font-family: inter;
@@ -235,12 +417,25 @@ import { useState } from 'react'
     color: #303030;
     margin-top: 50px;
     margin-bottom: 15px;
+    @media (max-width:1200px) {
+      font-size: 13px;
+    }
+    @media (max-width:750px) {
+      font-size: 10px;
+      margin: 0;
+    }
   `
   const WeekNumberBottomP=styled.p`
     font-family: inter;
     font-size: 56px;
     font-weight: 700;
     color: #303030;
+    @media (max-width:1200px) {
+      font-size: 40px;
+    }
+    @media (max-width:750px) {
+      font-size: 20px;
+    }
   `
 
 function App() {
